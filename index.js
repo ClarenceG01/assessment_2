49e9c76f9129eff0c5032c113482b09d3ba1f53b
ClarenceG01 <gatama001@gmail.com>
2023-05-19 11:15:58 +0300

    add delete and filter to js

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const products_url="https://jsonplaceholder.typicode.com/posts/1";
+const products_url = "https://jsonplaceholder.typicode.com/posts/1";
+const all_products="https://jsonplaceholder.typicode.com/posts";
+const delete_url = "https://jsonplaceholder.typicode.com/posts/1";
+const filter_url= 'https://jsonplaceholder.typicode.com/posts?userId=1'
 // LIST
-// fetch(products_url).then(res=>res.json().then(result=>console.log(result)))
-
+fetch(all_products).then(res=>res.json().then(result=>console.log(result)))
 
 // // POST
-// async function createUser(){
-//     try {
-//         const new_user= await fetch(products_url,{
-//             method:'POST',
-//             body: JSON.stringify({
-//                 userId:101,
-//                 title:'Doe',
-//                 body: 'lorem'
-//             }),
-//             headers: {
-//                 'Content-type': 'application/json; charset=UTF-8',
-//               },
-//         });
-//         console.log(new_user);
-//     } catch (error) {
-//         console.log(error)
-//     }
+// async function createUser() {
+//   try {
+//     const new_user = await fetch(products_url, {
+//       method: "POST",
+//       body: JSON.stringify({
+//         userId: 101,
+//         title: "Doe",
+//         body: "lorem",
+//       }),
+//       headers: {
+//         "Content-type": "application/json; charset=UTF-8",
+//       },
+//     });
+//     const json = await new_user.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.log(error);
+//   }
 // }
-// createUser()
+// createUser();
+
 
 
 //UPDATE
@@ -34,7 +38,7 @@ const products_url="https://jsonplaceholder.typicode.com/posts/1";
 //                 id: 1,
 //                 title: 'foo',
 //                 body: 'bar',
-//                 userId: 1,  
+//                 userId: 1,
 //             }),
 //             headers: {
 //                 'Content-type': 'application/json; charset=UTF-8',
@@ -46,29 +50,51 @@ const products_url="https://jsonplaceholder.typicode.com/posts/1";
 //     }
 // }
 // update()
-// async function update() {
-//     try {
-//       const response = await fetch(products_url, {
-//         method: 'PUT',
-//         body: JSON.stringify({
-//             id: 1,
-//             title: 'foo',
-//             body: 'bar',
-//             userId: 1,
-//           }),
-//         headers: {
-//           'Content-type': 'application/json; charset=UTF-8',
-//         },
-//       });
-//       const json = await response.json();
-//       console.log(json);
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   }
-  
-//   update();
+async function update() {
+  try {
+    const response = await fetch(products_url, {
+      method: "PUT",
+      body: JSON.stringify({
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
+update();
 
 // DELETE
-  
+async function deleteEntry() {
+  try {
+    const response = await fetch(delete_url, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+deleteEntry();
+
+// FILTER
+async function deleteEntry() {
+    try {
+      const response = await fetch(filter_url);
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  deleteEntry();
